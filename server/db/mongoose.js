ba0fdd1c7f0b96fc33370831538ed61bb1308abd
ashok <ashokreddykommasani@gmail.com>
2018-04-18 13:47:17 +0530

    changes for upload

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongodb://<dbuser>:<dbpassword>@ds149309.mlab.com:49309/ashokreddytestdatabase
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds149309.mlab.com:49309/ashokreddytestdatabase');
 
 module.exports = {mongoose};
