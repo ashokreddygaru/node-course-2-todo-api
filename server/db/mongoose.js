19479a6a713e6b4de2c41f53c3b1e4b0fadb41a9
ashok <ashokreddykommasani@gmail.com>
2018-04-18 13:52:25 +0530

    changes for upload

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //
-mongoose.connect('mongodb://ashokreddy261:@shok.0026K@ds149309.mlab.com:49309/ashokreddytestdatabase');
+mongoose.connect('mongodb://<ashokreddy261>:<@shok.0026K>@ds149309.mlab.com:49309/ashokreddytestdatabase');
 
 module.exports = {mongoose};
